1b3bc4cc3f5b4cdeb004cf2e856691a0b363f816
yemik <oduyemik@outlook.com>
2024-05-21 01:23:15 +0100

    Update custom_config.h with default miryoku config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_CLIPBOARD_MAC
+
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
